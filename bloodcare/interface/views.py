c0524d757e840995852a7074156895ab37344a8a
Niranjan B <niranjannb7777@gmail.com>
2021-06-26 17:14:07 +0530

    Req Change

--- bloodcare/interface/views.py
+++ bloodcare/interface/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 
 from rest_framework.views import APIView
-from rest_framwork import status
+from rest_framework import status
 
 
 class PhoneNumberView(APIView):
