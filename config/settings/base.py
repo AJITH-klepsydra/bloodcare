472d16b3bbf0d53f525753c58c318c6b332f3ed7
Niranjan B <niranjannb7777@gmail.com>
2021-06-27 16:04:47 +0530

    get method removed

--- config/settings/base.py
+++ config/settings/base.py
@@ -308,7 +308,7 @@ REST_FRAMEWORK = {
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
-CORS_URLS_REGEX = r"^/api/.*$"
+# CORS_URLS_REGEX = r"^/api/.*$"
 # Your stuff...
 # ------------------------------------------------------------------------------
 CORS_ALLOWED_ORIGINS = [
