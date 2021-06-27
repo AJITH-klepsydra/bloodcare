342750dc34224e1924553a4a971a06e6c400a960
Niranjan B <niranjannb7777@gmail.com>
2021-06-27 13:46:50 +0530

    status

--- bloodcare/donor/decorators.py
+++ bloodcare/donor/decorators.py
@@ -9,11 +9,11 @@ def is_authenticated(func):
             return func(obj, request, *args, **kwargs)
         token = request.headers.get('Authorization', None)
         if not token:
-            return Response({"info": "UnAuthenticated"}, status=401)
+            return Response({"message": "UnAuthenticated"}, status=401)
         try:
             request.user = Recipient.objects.get(key=token)
         except Exception:
-            return Response({"info": "UnAuthenticated"}, status=401)
+            return Response({"message": "UnAuthenticated"}, status=401)
         return func(obj, request, *args, **kwargs)
 
     return inner
--- bloodcare/interface/serializers.py
+++ bloodcare/interface/serializers.py
@@ -6,4 +6,4 @@ from .models import Recipient
 class RecipientSerializer(ModelSerializer):
     class Meta:
         model = Recipient
-        fields = ('phone_no', 'latitude', 'longitude', 'zip_code')
+        fields = ('phone_no', 'latitude', 'longitude', 'zip_code', 'blood_group')
